a3149295d293fc67f8daff3332c2ef1e077c12a1
Samriddhi Tiwary <samriddhi.tiwary01@gmail.com>
2024-08-05 16:03:18 +0530

    carousel bug fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header  from './components/Header/Header';
 import ClinicCarousel from './components/Carousel/Carousel';
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <>
